82a637eac8dfdc003b81a911924240bc5a425caf
saim514 <81713694+saim514@users.noreply.github.com>
2022-05-21 14:57:46 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+noseY=0;
+difference = 0;
+leftwristX = 0;
+rightwristX = 0;
+
 function setup()
 {
     video = createCapture(VIDEO);
@@ -10,21 +16,33 @@ function setup()
     posenet.on('poses', gotPoses);
 }
 
-function draw()
-{
-    background('#1976D2')
-}
-
 function modelLoaded()
 {
     console.log("Very good website for stretching arms!")
 }
 
-function gotPoses()
+function draw()
+{
+    background('#1976D2');
+    document.getElementById("square_side").innerHTML = "Squares length and width is: " + difference + "px";
+    fill("#84FFFF");
+    stroke("#84FFFF");
+    square(noseX, noseY, difference);
+}
+
+function gotPoses(results)
 {
     if (results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X = " + noseX + " and Nose Y = " + noseY);
+
+        leftwristX = results[0].pose.leftWrist.x;
+        rightwristX = results[0].pose.rightWrist.x;
+        difference = floor(leftwristX - rightwristX);
+        console.log("Left wrist X = " + leftwristX + ", right wrist X = " + rightwristX + ", and difference = " + difference);
     } 
 }
 
